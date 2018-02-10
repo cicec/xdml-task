83bd1fb249d7be6b919931bc7b3817e90a01ad04
clancysong <clancysong@gmail.com>
2018-02-10 09:58:58 +0800

    correct insertion sort

--- task14/sort.js
+++ task14/sort.js
@@ -4,9 +4,9 @@ function bubbleSort(paramArr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i; j++) {
             if (arr[j] > arr[j + 1]) {
-                const tmp = arr[j]
+                const temp = arr[j]
                 arr[j] = arr[j + 1]
-                arr[j + 1] = tmp
+                arr[j + 1] = temp
             }
         }
     }
@@ -33,12 +33,13 @@ function selectionSort(paramArr) {
 function insertionSort(paramArr) {
     const arr = paramArr.slice()
     for (let i = 1; i < arr.length; i++) {
-        const value = arr[i]
-        for (let j = i - 1; j >= 0; j--) {
-            if (arr[j] > value) {
-                arr[j + 1] = arr[j]
+        for (let j = i; j > 0; j--) {
+            if (arr[j - 1] > arr[j]) {
+                const temp = arr[j]
+                arr[j] = arr[j - 1]
+                arr[j - 1] = temp
             } else {
-                arr[j + 1] = value
+                break
             }
         }
     }
